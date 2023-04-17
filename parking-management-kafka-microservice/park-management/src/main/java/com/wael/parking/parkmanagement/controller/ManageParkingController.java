76fd49b3b283d474836a2d53be232e35b3355bcb
Ouni <waelounie@gmail.com>
2023-04-17 20:09:19 +0200

    0000 - create modules for car van and motocycle producers

---
+++ parking-management-kafka-microservice/park-management/src/main/java/com/wael/parking/parkmanagement/controller/ManageParkingController.java
@@ -0,0 +1,10 @@
+package com.wael.parking.parkmanagement.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1")
+public class ManageParkingController {
+
+}
---
+++ parking-management-kafka-microservice/van-microservice/src/main/java/com/wael/parking/vanservice/VanServiceApplication.java
@@ -0,0 +1,13 @@
+package com.wael.parking.vanservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VanServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VanServiceApplication.class, args);
+	}
+
+}
